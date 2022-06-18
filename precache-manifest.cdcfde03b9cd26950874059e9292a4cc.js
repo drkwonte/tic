f9249bdc1709d3f3ae263c8e144fe8ce5b4d1d69
drkwonte <66548996+drkwonte@users.noreply.github.com>
2022-06-18 09:00:57 +0900

    Add files via upload

---
+++ precache-manifest.cdcfde03b9cd26950874059e9292a4cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b666bfed29bfa6337954",
+    "url": "/css/app.e53f8c73.css"
+  },
+  {
+    "revision": "7177d81322ea85ad7a302f3abeccccf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b666bfed29bfa6337954",
+    "url": "/js/app.3bb9d66c.js"
+  },
+  {
+    "revision": "34060854bcd3dceebb92",
+    "url": "/js/chunk-vendors.b9a096ee.js"
+  },
+  {
+    "revision": "01e9d5b735f38379740432d87091d78d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
